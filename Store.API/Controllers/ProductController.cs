01f0f95feb7d85d9e09ff039b5651aa6dd216948
sya <syavash.36@gmail.com>
2022-01-12 19:33:32 +0330

    Auto migration and Seed Data

--- Store.API/Controllers/ProductController.cs
+++ Store.API/Controllers/ProductController.cs
@@ -1,16 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Store.Core.Products.Entity;
 using Store.Core.Products.ServiceContract;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Store.API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
+
     public class ProductController : BaseApiController
     {
         private readonly IProductService _productService;
--- Store.API/Program.cs
+++ Store.API/Program.cs
@@ -1,11 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Store.Infrastructure.Data;
 
 namespace Store.API
 {
@@ -13,7 +10,24 @@ namespace Store.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            MigrateAndSeedDb(host);
+            host.Run();
+        }
+
+        private static void MigrateAndSeedDb(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+                var logger = loggerFactory.CreateLogger<StoreContextInitialWork>();
+                var context = services.GetRequiredService<StoreDbContext>();
+
+                StoreContextInitialWork.Migrate(context, logger);
+                StoreContextInitialWork.SeedData(context, logger);
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
