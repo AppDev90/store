4b2e5acf2bdb7f0dda6274af4108cd94d7dac148
sya <syavash.36@gmail.com>
2022-01-15 13:51:07 +0330

    bug fixed

--- Store.ApplicationService/Errors/BaseTypes/SingleError.cs
+++ Store.ApplicationService/Errors/BaseTypes/SingleError.cs
@@ -5,7 +5,7 @@ namespace Store.ApplicationService.Errors.BaseTypes
     public abstract class SingleError : Exception
     {
         private string _error;
-        public void Throw(string error)
+        public virtual void Throw(string error)
         {
             _error = error;
             throw this;
